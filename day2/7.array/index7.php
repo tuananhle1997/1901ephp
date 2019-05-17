58537e5d1cfefde9532336aa449727166ab57b39
tuananhle1997 <tuananhle250597@gmail.com>
2019-05-17 08:41:13 +0700

    1111

--- day2/7.array/index7.php
+++ day2/7.array/index7.php
@@ -9,7 +9,24 @@
 
 <?php
     $student = array("tuấn", "hùng", "nam", "linh", "chi");
+    echo "<br> trước khi sắp xếp";
+    echo "<pre>";
+    print_r($student);
+    echo "</pre>";
     echo "<br> đếm số phần tử trong mảng: " .count($student);
+    asort($student);
+    echo "<br> sau khi sắp xếp asort() sắp xếp mảng theo chỉ số tăng dần theo AZ dựa vào các giá trị phần tử";
+
+    echo "<pre>";
+    print_r($student);
+    echo "</pre>";
+
+    rsort($student);
+    echo "<br> sau khi sắp xếp asort() sắp xếp mảng theo chỉ số giảm dần theo AZ dựa vào các giá trị phần tử";
+    echo "<pre>";
+    print_r($student);
+    echo "</pre>";
+
 ?>
 </body>
 </html>
