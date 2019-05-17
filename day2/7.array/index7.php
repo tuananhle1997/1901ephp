2b1c475d609baba154d30758266835ca98b07441
tuananhle1997 <tuananhle250597@gmail.com>
2019-05-17 08:28:30 +0700

    1111

---
+++ day2/7.array/index7.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>các hàm xử lý mảng</h1>
+
+<?php
+    $student = array("tuấn", "hùng", "nam", "linh", "chi");
+    echo "<br> đếm số phần tử trong mảng: " .count($student);
+?>
+</body>
+</html>
